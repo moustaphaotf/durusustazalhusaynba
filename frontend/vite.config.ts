5acffaa4ceedfed2eedbda9db0024c2fce058e5c
Mamadou Moustapha Diallo <moustaphaotf@gmail.com>
2026-07-18 11:19:22 +0000

    Chore(frontend): remove proxy config on vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,19 +6,15 @@ import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import viteReact from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-const apiTarget = process.env.VITE_API_BASE_URL ?? 'http://localhost:8000'
 
 const config = defineConfig({
   resolve: { tsconfigPaths: true },
   server: {
     host: '0.0.0.0',
     port: 3000,
-    proxy: {
-      '/api': {
-        target: apiTarget,
-        changeOrigin: true,
-      },
-    },
+    allowedHosts: [
+      'durusustazalhusaynba.devtutor.site',
+    ]
   },
   plugins: [devtools(), tailwindcss(), tanstackStart(), viteReact()],
   test: {
