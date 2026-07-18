3dbbd7ea80f8b2610a69ec9c9ed241fa875a79c8
Mamadou Moustapha Diallo <moustaphaotf@gmail.com>
2026-07-18 11:51:38 +0000

    Chore(frontend build): added nitro as agnosic deployment layer

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,18 +5,16 @@ import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 
 import viteReact from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-
+import { nitro } from 'nitro/vite'
 
 const config = defineConfig({
   resolve: { tsconfigPaths: true },
   server: {
     host: '0.0.0.0',
     port: 3000,
-    allowedHosts: [
-      'durusustazalhusaynba.devtutor.site',
-    ]
+    allowedHosts: ['durusustazalhusaynba.devtutor.site'],
   },
-  plugins: [devtools(), tailwindcss(), tanstackStart(), viteReact()],
+  plugins: [devtools(), tailwindcss(), tanstackStart(), nitro(), viteReact()],
   test: {
     environment: 'jsdom',
     include: ['src/**/*.test.{ts,tsx}'],
